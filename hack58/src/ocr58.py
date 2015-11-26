ff1ace94bb5757b2f6d012ed0929c65df0d54e05
cuittzq <455164259@qq.com>
2015-11-26 17:39:02 +0800

    修改为破解517的验证码

--- hack58/src/ocr58.py
+++ hack58/src/ocr58.py
@@ -1,12 +1,14 @@
-#coding=utf-8
-import Image  
-import ImageEnhance  
-import ImageFilter  
-import sys  
+# coding=utf-8
+import Image
+import ImageEnhance
+import ImageFilter
+import sys
 import ImageDraw
 from StringIO import StringIO
 import copy
 import json
+import random
+import urllib
 
 from checkCharData import show
 
@@ -19,9 +21,11 @@ with open('data.json') as f:
 '''
  二值化
 '''
+
+
 def calcThreshold(img):
-    im=Image.open(img)
-    
+    im = Image.open(img)
+
     L = im.convert('L').histogram()
     sum = 0
     threshold = 0
@@ -30,11 +34,12 @@ def calcThreshold(img):
         if sum >= 530:
             threshold = i
             break
-#    if threshold > 105:
-#        threshold = 105
+            #    if threshold > 105:
+            #        threshold = 105
     return threshold
 
-def binaryzation(img,threshold = 90):
+
+def binaryzation(img, threshold=90):
     table = []
     for i in range(256):
         if i < threshold:
@@ -43,12 +48,12 @@ def binaryzation(img,threshold = 90):
             table.append(1)
 
     print type(img)
-    if not isinstance(img,StringIO) and type(img) != str and type(img) != unicode:
+    if not isinstance(img, StringIO) and type(img) != str and type(img) != unicode:
         raise Exception('img must be StringIO or filename(str/unicode)')
-    im=Image.open(img)
+    im = Image.open(img)
     imgry = im.convert('L')
     imgry.save("bi0.bmp")
-    imout = imgry.point(table,'1')  
+    imout = imgry.point(table, '1')
     imout.save("bi.bmp")
     return imout
 
@@ -56,106 +61,110 @@ def binaryzation(img,threshold = 90):
 '''
 抽取出字符矩阵 列表
 '''
+
+
 def extractChar(im):
-    OFFSETLIST = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,1),(1,-1),(-1,-1)]
+    OFFSETLIST = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
     pixelAccess = im.load()
     num = 1
     queue = []
-    ff = [[0]*im.size[1] for i in xrange(im.size[0])]
-#   打印原始
-#    for j in xrange(im.size[1]):
-#        for i in xrange(im.size[0]):
-#            if pixelAccess[i,j]:
-#                print ' ',
-#            else:
-#                print 'O',
-#        print '\n'
-        
+    ff = [[0] * im.size[1] for i in xrange(im.size[0])]
+    #   打印原始
+    # for j in xrange(im.size[1]):
+    #     for i in xrange(im.size[0]):
+    #         if pixelAccess[i, j]:
+    #             print ' ',
+    #         else:
+    #             print 'O',
+    #     print '\n'
+
     '''
         floodfill 提出块
     '''
-    
+
     for i in xrange(im.size[0]):
         for j in xrange(im.size[1]):
             '''
                 pixelAccess[i,j] == 0 表示是黑点
             '''
-            if pixelAccess[i,j] == 0 and ff[i][j] == 0:
+            if pixelAccess[i, j] == 0 and ff[i][j] == 0:
                 ff[i][j] = num
-                queue.append((i,j))
-                while len(queue) > 0 :
-                    a,b = queue[0]
+                queue.append((i, j))
+                while len(queue) > 0:
+                    a, b = queue[0]
                     queue = queue[1:]
-                    for offset1,offset2 in OFFSETLIST:
-                        x,y = a + offset1, b + offset2
-                        if x < 0 or x >= im.size[0]:continue
-                        if y < 0 or y >= im.size[1]:continue
-                        if pixelAccess[x,y] == 0 and ff[x][y] == 0:
+                    for offset1, offset2 in OFFSETLIST:
+                        x, y = a + offset1, b + offset2
+                        if x < 0 or x >= im.size[0]: continue
+                        if y < 0 or y >= im.size[1]: continue
+                        if pixelAccess[x, y] == 0 and ff[x][y] == 0:
                             ff[x][y] = num
-                            queue.append((x,y))
+                            queue.append((x, y))
 
                 num += 1
-#   打印聚类 
-#    for j in xrange(im.size[1]):
-#        for i in xrange(im.size[0]):
-#            print ' ' if ff[i][j] == 0 else ff[i][j],
-#        print '\n'
-#    print num
-    
+                #   打印聚类
+                # for j in xrange(im.size[1]):
+                #     for i in xrange(im.size[0]):
+                #         print ' ' if ff[i][j] == 0 else ff[i][j],
+                #     print '\n'
+                # print num
+
     '''
         字符点阵的坐标列表，对齐到 (0,0)
         eg: [(1,2),(3,24),(54,23)]
     '''
-    #初始化字符数组
+    # 初始化字符数组
     info = {
-            "x_min":im.size[0],
-            "y_min":im.size[1],
-            "x_max":0,
-            "y_max":0,
-            "width":0,
-            "height":0,
-            "number":0,
-            "points":[]
+        "x_min": im.size[0],
+        "y_min": im.size[1],
+        "x_max": 0,
+        "y_max": 0,
+        "width": 0,
+        "height": 0,
+        "number": 0,
+        "points": []
     }
     charList = [copy.deepcopy(info) for i in xrange(num)]
-    #统计
+    # 统计
     for i in xrange(im.size[0]):
         for j in xrange(im.size[1]):
             if ff[i][j] == 0:
                 continue
             id = ff[i][j]
-            if i < charList[id]['x_min']:charList[id]['x_min'] = i
-            if j < charList[id]['y_min']:charList[id]['y_min'] = j
-            if i > charList[id]['x_max']:charList[id]['x_max'] = i
-            if j > charList[id]['y_max']:charList[id]['y_max'] = j
+            if i < charList[id]['x_min']: charList[id]['x_min'] = i
+            if j < charList[id]['y_min']: charList[id]['y_min'] = j
+            if i > charList[id]['x_max']: charList[id]['x_max'] = i
+            if j > charList[id]['y_max']: charList[id]['y_max'] = j
             charList[id]['number'] += 1
-            charList[id]['points'].append((i,j))
+            charList[id]['points'].append((i, j))
     for i in xrange(num):
         charList[i]['width'] = charList[i]['x_max'] - charList[i]['x_min'] + 1
         charList[i]['height'] = charList[i]['y_max'] - charList[i]['y_min'] + 1
-        #修正偏移
-        charList[i]['points'] = [(x-charList[i]['x_min'], y-charList[i]['y_min']) for x,y in charList[i]['points'] ]
-    #过滤杂点
+        # 修正偏移
+        charList[i]['points'] = [(x - charList[i]['x_min'], y - charList[i]['y_min']) for x, y in charList[i]['points']]
+    # 过滤杂点
     ret = [one for one in charList if one['number'] > 4]
-    #排序
-    ret.sort(lambda a,b:a['x_min'] < b['x_min'])
+    # 排序
+    ret.sort(lambda a, b: a['x_min'] < b['x_min'])
     return ret
+
 
 '''
     识别字符
 '''
 
-def charSimilarity(charA,charB):
-    s2 = set([(one[0],one[1]) for one in charB['points']])
+
+def charSimilarity(charA, charB):
+    s2 = set([(one[0], one[1]) for one in charB['points']])
     sumlen = len(charA['points']) + len(charB['points'])
     max = 0
     # 晃动匹配
     i_adjust = 1 if charB['width'] - charA['width'] >= 0 else -1
     j_adjust = 1 if charB['height'] - charA['height'] >= 0 else -1
-    for i in xrange(0,charB['width'] - charA['width'] + i_adjust,i_adjust):
-        for j in xrange(0,charB['height'] - charA['height'] + j_adjust,j_adjust):
-            s1 = set([(one[0]+i,one[1]+j) for one in charA['points']])
-            sim = len(s1&s2) *2.0 / sumlen
+    for i in xrange(0, charB['width'] - charA['width'] + i_adjust, i_adjust):
+        for j in xrange(0, charB['height'] - charA['height'] + j_adjust, j_adjust):
+            s1 = set([(one[0] + i, one[1] + j) for one in charA['points']])
+            sim = len(s1 & s2) * 2.0 / sumlen
             if sim > max:
                 max = sim
     return max
@@ -165,60 +174,94 @@ def recognise(one):
     max = 0
     ret = None
     for char in CharMatrix:
-        s = charSimilarity(one,CharMatrix[char])
-        #print s * 100,"%"
+        s = charSimilarity(one, CharMatrix[char])
+        # print s * 100,"%"
         if s > max:
             ret = char
             max = s
     return ret
-    
 
 
 '''
     识别验证码
 '''
+
+
 def DoWork(img):
     ans = []
     threshold = calcThreshold(img)
-    print 'threshold:',threshold
-    im = binaryzation(img,threshold)
+    print 'threshold:', threshold
+    im = binaryzation(img, threshold)
     chars = extractChar(im)
     for one in chars:
         ans.append(recognise(one))
     return ans
 
+
 '''
     获取字模
 '''
-def dump(char,dic):
-    with open('../json/'+ char + '.json','wb') as f:
+
+
+def dump(char, dic):
+    with open('../json/' + char + '.json', 'wb') as f:
         f.write(json.dumps(dic))
-        
+
+
+
+        # 传入图片地址，文件名，保存单张图片
+
+
+def saveImg(imageURL, fileName):
+    f = []
+    try:
+        u = urllib.urlopen(imageURL)
+        data = u.read()
+        f = open(fileName, 'wb')
+        f.write(data)
+        print fileName
+        f.close()
+    except:
+        print "Unexpected error:", sys.exc_info()[2]
+    finally:
+        f.close()
+
+
 def GETSTAND():
     ans = []
     im = binaryzation('../pic/Ta2px.bmp')
     for one in extractChar(im):
         ans.append(one)
-    print 'LAST:',len(ans)
+    print 'LAST:', len(ans)
     if len(ans) != 5:
         print '!!!!!!!!!!! ERROR !!!!!!!!!!!!!'
     else:
-        #dump('K',ans[0])
-        #dump('_k',ans[1])
-        dump('Z',ans[2])
-        #dump('Y',ans[3])
-        #dump('K',ans[4])
-    
+        # dump('K',ans[0])
+        # dump('_k',ans[1])
+        dump('Z', ans[2])
+        # dump('Y',ans[3])
+        # dump('K',ans[4])
+
+
+def Get517validatecode():
+    for i in range(10000):
+        imgurl = 'http://www.517na.com/Images/ValidateCode.aspx?' + str(random.random())
+        filename = 'D://517na/pic' + str(i) + '.png'
+        saveImg(imgurl, filename);
+
+
 def main():
-    ans = DoWork('../pic/rrrrrrrsrrrrr.bmp')
-    print ans
+    for i in range(10000):
+        filename = 'D://517na/pic' + str(i) + '.png'
+        print filename
+        ans = DoWork(filename)
+        anstsr = ''
+        for index in ans:
+            anstsr+=index
+        print anstsr
+
 
 if __name__ == '__main__':
     main()
-    #GETSTAND()
-    #calcThreshold('../pic/Ta2px.bmp')
-        
-                
-        
-
-
+    # GETSTAND()
+    # calcThreshold('../pic/Ta2px.bmp')
